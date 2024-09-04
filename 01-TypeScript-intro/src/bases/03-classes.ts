3eb5513d6f5095d41d2b5a7575488c110804c4c3
Santiago Lopez <slopezr10@miumg.edu.gt>
2024-09-04 17:44:11 -0600

    Metodos en typescript

--- 01-TypeScript-intro/src/bases/03-classes.ts
+++ 01-TypeScript-intro/src/bases/03-classes.ts
@@ -1,11 +1,29 @@
 export class Pokemon {
+    get imageUrl(): string {
+        return `https://pokemon.com/${ this.id }.jpg`;
+    }
     constructor(
         public readonly id: number, 
-        public name: string
+        public name: string,
+        // public imageUrl: string,
     ){}
+
+    // Metodos
+    public scream () {
+        console.log(`${ this.name.toUpperCase() }!!!`);
+        this.speak()
+    }
+
+    private speak() {
+        console.log(`${ this.name}, ${this.name}`);
+    }
 }
 
 export const charmander = new Pokemon(4, 'Charmander')
 
 // charmander.id = 5;
 // charmander.name = 'Mew';
+
+console.log(charmander);
+charmander.scream();
+// charmander.speak();
