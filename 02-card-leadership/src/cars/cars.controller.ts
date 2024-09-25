f3c9e7a43755e7b844d4cf9e0682ac4a4d8dc408
Santiago Lopez <slopezr10@miumg.edu.gt>
2024-09-24 20:20:42 -0600

    40 - Post, Patch y Delete

--- 02-card-leadership/src/cars/cars.controller.ts
+++ 02-card-leadership/src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -13,8 +13,29 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById( @Param('id') id: string ) {
+    getCarById( @Param('id', ParseIntPipe) id: number ) {
         console.log({id})
-        return this.carsService.findOneById(+id);
+        return this.carsService.findOneById(id);
+    }
+
+    @Post()
+    create(@Body() body: any) {
+        return body;
+    }
+
+    @Patch(':id')
+    updateCar(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() body: any) 
+    {
+        return body;
+    }
+
+    @Delete(':id')
+    deleteCar( @Param('id', ParseIntPipe) id: number) {
+        return {
+            method: 'delete',
+            id: id
+        }
     }
 }
